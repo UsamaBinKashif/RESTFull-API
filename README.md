441c1c4d253ab659e80a5f57b4f104153410694e
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-14 00:21:40 +0500

    All tasks done

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-RESTFULL API JSON
+## RESTFULL API using Node JS, Express JS and Raw JSON.
 
 
-GET /api/users - get all users
-GET /users - Generate the HTML DOC 
-GET /api/users/ID- get the user of given ID for suppose ID:1
+1
+- GET /api/users - get all users
 
-POST /api/users - to create a new user
+2
+- GET /users - Generate the HTML DOC 
 
-PATCH /api/users/ID- to update the user of given ID
+3
+- GET /api/users/ID- get the user of given ID for suppose ID:1
 
-DELETE /api/users/ID - to delete the user of given ID 
+4
+- POST /api/users - to create a new user
+
+5
+- PATCH /api/users/ID- to update the user of given ID
+
+6
+- DELETE /api/users/ID - to delete the user of given ID 
--- index.js
+++ index.js
@@ -46,23 +46,44 @@ app.post("/api/users", (req, res) => {
     return res.json({ status: "ok", id: users.length });
   });
 });
+
 /*
-Task:6
+Task:5
 edit the existing user also learned about status codes here
 */
-app.patch('/api/users/:id', (req, res) => {
+app.patch("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
-  const users = JSON.parse(fs.readFileSync('./USERS_DATA.json', 'utf-8'));
-  const user = users.find(user => user.id === id);
-  if (!user) return res.status(404).json({ status: 'error', message: 'User not found' });
+  const users = JSON.parse(fs.readFileSync("./USERS_DATA.json", "utf-8"));
+  const user = users.find((user) => user.id === id);
+  if (!user)
+    return res.status(404).json({ status: "error", message: "User not found" });
   Object.assign(user, req.body);
-  fs.writeFile('./USERS_DATA.json', JSON.stringify(users), err => {
-    return res.status(err ? 500 : 200).json({ status: err ? 'error' : 'ok', data: user });
+  fs.writeFile("./USERS_DATA.json", JSON.stringify(users), (err) => {
+    return res
+      .status(err ? 500 : 200)
+      .json({ status: err ? "error" : "ok", data: user });
   });
 });
 
+/*
+Task:6
+delete the user
+*/
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const users = JSON.parse(fs.readFileSync("./USERS_DATA.json", "utf-8"));
+  const user = users.findIndex((user) => user.id === id);
+  
+  if (!user || user === -1)
+    return res.status(404).json({ status: "error", message: "User not found" });
 
-
+  users.splice(user, 1);
+  fs.writeFile("./USERS_DATA.json", JSON.stringify(users), (err) => {
+    return res
+      .status(err ? 500 : 200)
+      .json({ status: err ? "error" : "deleted"});
+  });
+});
 
 //starting our app
 app.listen(port, () => {
