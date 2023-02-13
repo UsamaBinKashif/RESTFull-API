393294353e4f2686c2e16146ce8f6f1d4607c828
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-13 22:44:56 +0500

    Status Code 200

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/users", (req, res) => {
   res.send(html);
 });
 /*
-Task:2
+Task:3
 get the user of given ID for suppose ID:1
 */
 app.get("/api/users/:id", (req, res) => {
@@ -36,16 +36,33 @@ app.get("/api/users/:id", (req, res) => {
 });
 
 /*
-Task:3
+Task:4
 create a new user
 */
 app.post("/api/users", (req, res) => {
   const body = req.body;
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./USERS_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "ok", id: users.length + 1 });
+    return res.json({ status: "ok", id: users.length });
   });
 });
+/*
+Task:6
+edit the existing user also learned about status codes here
+*/
+app.patch('/api/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const users = JSON.parse(fs.readFileSync('./USERS_DATA.json', 'utf-8'));
+  const user = users.find(user => user.id === id);
+  if (!user) return res.status(404).json({ status: 'error', message: 'User not found' });
+  Object.assign(user, req.body);
+  fs.writeFile('./USERS_DATA.json', JSON.stringify(users), err => {
+    return res.status(err ? 500 : 200).json({ status: err ? 'error' : 'ok', data: user });
+  });
+});
+
+
+
 
 //starting our app
 app.listen(port, () => {
